0210500032cac4606423bec65dbadeaa000cfdc5
SadmanUddin <sadmanuddin123@gmail.com>
2025-04-06 12:27:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("What's your first name? ");
+        string  D = Console.ReadLine();
+        Console.WriteLine($"HELLO {D.ToUpper()}");
+    }
+}
